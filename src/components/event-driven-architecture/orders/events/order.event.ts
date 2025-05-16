ff30c07ee17f6a878d84729a86043ac745fe1a57
viktorhugo <viktor1103@gmail.com>
2025-05-16 08:16:37 -0500

    updates

--- src/components/event-driven-architecture/orders/events/order.event.ts
+++
@@ -1,9 +0,0 @@
-// events/order-created.event.ts
-export class OrderCreatedEvent {
-    constructor(public readonly order: any) {}
-}
-
-export class PaymentProcessedEvent {
-    constructor(public readonly order: any) {}
-}
-
